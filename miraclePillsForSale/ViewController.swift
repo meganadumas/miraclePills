746fac9b3d43fa447870b4294908ea351aae58ab
Your Name <megan.a.dumas@gmail.com>
2017-04-10 11:33:49 -0700

    Initial Commit

---
+++ miraclePillsForSale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miraclePillsForSale
+//
+//  Created by Megan Dumas on 4/10/17.
+//  Copyright © 2017 Megan Dumas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
